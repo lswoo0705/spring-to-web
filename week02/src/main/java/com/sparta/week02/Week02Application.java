04b8c8374f2808ddcccd92f580c8ef1684bf6447
sangwoo lee <lswoo0705@gmail.com>
2022-12-13 23:56:00 +0900

    2주차 강의 2-6 완성

--- week02/src/main/java/com/sparta/week02/Week02Application.java
+++ week02/src/main/java/com/sparta/week02/Week02Application.java
@@ -6,9 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.List;
 
+@EnableJpaAuditing  // 이게 있어야 수정일자가 스프링을 통해 반영이 된다.
 @SpringBootApplication
 public class Week02Application {
 
