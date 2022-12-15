17db2c35ec1822c09059ddf2065f9b22b48c1c43
sangwoo lee <lswoo0705@gmail.com>
2022-12-15 10:24:16 +0900

    spring 기초 강의 3-5 완료

---
+++ week03/src/main/java/com/sparta/week03/controller/MemoController.java
@@ -0,0 +1,40 @@
+package com.sparta.week03.controller;
+
+import com.sparta.week03.domain.Memo;
+import com.sparta.week03.domain.MemoRepository;
+import com.sparta.week03.domain.MemoRequestDto;
+import com.sparta.week03.service.MemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class MemoController {
+    private final MemoRepository memoRepository;  // 생성, 조회, 삭제
+    private final MemoService memoService;    // 업데이트
+
+    @PostMapping("/api/memos")
+    public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
+        Memo memo = new Memo(requestDto);
+        return memoRepository.save(memo);
+    }
+
+    @GetMapping("/api/memos")
+    public List<Memo> readMemo() {
+        return memoRepository.findAllByOrderByModifiedAtDesc();
+    }
+
+    @PutMapping("/api/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        memoService.update(id, requestDto);
+        return id;
+    }
+
+    @DeleteMapping("/api/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        memoRepository.deleteById(id);
+        return id;
+    }
+}
