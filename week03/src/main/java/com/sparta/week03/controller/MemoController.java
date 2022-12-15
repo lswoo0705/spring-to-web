28dff877745d6c6c8cfe630cecddf22b65f2ea58
sangwoo lee <lswoo0705@gmail.com>
2022-12-15 15:26:56 +0900

    spring 기초 강의 3주차 숙제 완료

--- week03/src/main/java/com/sparta/week03/controller/MemoController.java
+++ week03/src/main/java/com/sparta/week03/controller/MemoController.java
@@ -7,6 +7,7 @@ import com.sparta.week03.service.MemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -23,7 +24,7 @@ public class MemoController {
 
     @GetMapping("/api/memos")
     public List<Memo> readMemo() {
-        return memoRepository.findAllByOrderByModifiedAtDesc();
+        return memoRepository.findAllByModifiedAtBetweenOrderByModifiedAtDesc(LocalDateTime.now().minusDays(1), LocalDateTime.now());
     }
 
     @PutMapping("/api/memos/{id}")
